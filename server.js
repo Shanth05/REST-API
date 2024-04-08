6d887f72abe8057611fc0a83a8a3efa07c6f5be8
shanth05 <shanth.pirashanth5@gmail.com>
2024-04-08 22:48:45 +0530

    Testing

--- server.js
+++ server.js
@@ -7,6 +7,10 @@ app.get('/', function (req, res) {
     res.send('Hello NODE API');
 });
 
+app.get('/blog', function (req, res) {
+    res.send('Hello Blog, My name is Pirashanth');
+});
+
 app.listen(3000, () => {
     console.log('Node API app is running on port 3000');
 });
